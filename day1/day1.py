650ee7161f523221d591fc444b83d5dc6cc09454
oliviergf <olivier.gauthier.farley@hotmail.com>
2019-12-01 11:31:54 -0500

    completes day1

--- day1/day1.py
+++ day1/day1.py
@@ -1,5 +1,37 @@
-print("yes this is day1 but ill do it later because too hangover")
-a = 10
+import math
+inputs = open("day1/input.txt", "r")
+lines = inputs.readlines()
 
 
-print(a)
+def calcFuel(lines):
+    totalFuel = 0
+    for line in lines:
+        totalFuel += math.floor(int(line) / 3) - 2
+    return totalFuel
+
+
+def recursiveFuel(fuel):
+    addedFuel = math.floor(fuel / 3) - 2
+    if addedFuel <= 0:
+        return 0
+    else:
+        return addedFuel + recursiveFuel(addedFuel)
+
+
+def calcFuelPart2(lines):
+    totalFuel = 0
+    for line in lines:
+        totalFuel += recursiveFuel(int(line))
+    return totalFuel
+
+
+def part1(lines):
+    print(calcFuel(lines))
+
+
+def part2(lines):
+    print(calcFuelPart2(lines))
+
+
+part1(lines)
+part2(lines)
