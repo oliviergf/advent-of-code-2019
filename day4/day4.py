edea58b3431146e611d7a5a5d94322694956c37e
oliviergf <olivier.gauthier.farley@hotmail.com>
2019-12-04 15:49:16 -0500

    cleans code

--- day4/day4.py
+++ day4/day4.py
@@ -3,44 +3,9 @@ line = inputs.readline()
 inputRange = list(map(lambda x: int(x), line.split("-")))
 
 
-def part1(inputRange):
-    nbOfValidPassword = 0
-    for i in range(inputRange[0], inputRange[1]):
-        number = str(i)
-        index = 0
-        hasConsec = False
-        neverDescrese = False
-        for y in range(len(number)):
-            if int(number[y]) < index:
-                break
-            if index == int(number[y]):
-                hasConsec = True
-            if y == len(number) - 1:
-                neverDescrese = True
-            index = int(number[y])
-
-        if(hasConsec and neverDescrese):
-            nbOfValidPassword += 1
-
-    print(nbOfValidPassword)
-
-
-def hasUnique(consecutives):
-    uniques = {}
-    for i in consecutives:
-        if i not in uniques:
-            uniques[i] = 1
-        else:
-            uniques[i] += 1
-    hasSingle = False
-    for key in uniques:
-        if(uniques[key] == 1):
-            hasSingle = True
-    return hasSingle
-
-
-def part2(inputRange):
-    nbOfValidPassword = 0
+def part1and2(inputRange):
+    nbOfValidPasswordPart1 = 0
+    nbOfValidPasswordPart2 = 0
     for i in range(inputRange[0], inputRange[1]):
         number = str(i)
         index = 0
@@ -57,11 +22,27 @@ def part2(inputRange):
                 neverDescrese = True
             index = int(number[y])
 
+        if(hasConsec and neverDescrese):
+            nbOfValidPasswordPart1 += 1
         if hasConsec and neverDescrese and hasUnique(groupOfConsecutives):
-            nbOfValidPassword += 1
+            nbOfValidPasswordPart2 += 1
 
-    print(nbOfValidPassword)
+    print(nbOfValidPasswordPart1)
+    print(nbOfValidPasswordPart2)
 
 
-part1(inputRange)
-part2(inputRange)
+def hasUnique(consecutives):
+    uniques = {}
+    for i in consecutives:
+        if i not in uniques:
+            uniques[i] = 1
+        else:
+            uniques[i] += 1
+    hasSingle = False
+    for key in uniques:
+        if(uniques[key] == 1):
+            hasSingle = True
+    return hasSingle
+
+
+part1and2(inputRange)
