a650685e25cc7d20fde3c1df56af2b13d7f530ac
oliviergf <olivier.gauthier.farley@hotmail.com>
2019-12-13 14:10:14 -0500

    refactors intcomputer

--- day5/day5.py
+++ day5/day5.py
@@ -14,62 +14,56 @@ def part1(inputs):
 def part2(inputs):
     commands = list(map(int, inputs))
     onlyInput = 5
-    intComputer_part2(commands, 0, onlyInput)
+    outputs = []
+    intComputer_part2(commands, 0, onlyInput, outputs)
+    print(outputs[0])
 
 
-def intComputer_part2(commands, index, onlyInput):
-    reverse_instruction = str(commands[index])[::-1]
-    while(len(reverse_instruction) is not 5):
-        reverse_instruction += "0"
+def intComputer_part2(commands, index, onlyInput, outputs):
+    instruction = str(commands[index])[::-1]
+    while(len(instruction) is not 5):
+        instruction += "0"
 
-    instruction = reverse_instruction[0]
+    op_code = instruction[0]
 
-    if instruction is not '9':
-        if instruction == '5':
-            first = commands[commands[index+1]
-                             ] if reverse_instruction[2] == '0' else commands[index+1]
-            second = commands[commands[index+2]
-                              ] if reverse_instruction[3] == '0' else commands[index+2]
+    if op_code is not '9':
+        first = commands[commands[index+1]
+                         ] if instruction[2] == '0' else commands[index+1]
+        second = commands[commands[index+2]
+                          ] if instruction[3] == '0' else commands[index+2]
+        if op_code == '5':
             pos_to_jump_to = second if first != 0 else index + 3
-            intComputer_part2(commands, pos_to_jump_to, onlyInput)
-        elif instruction == '6':
-            first = commands[commands[index+1]
-                             ] if reverse_instruction[2] == '0' else commands[index+1]
-            second = commands[commands[index+2]
-                              ] if reverse_instruction[3] == '0' else commands[index+2]
+            intComputer_part2(commands, pos_to_jump_to, onlyInput, outputs)
+        elif op_code == '6':
             pos_to_jump_to = second if first == 0 else index + 3
-            intComputer_part2(commands, pos_to_jump_to, onlyInput)
-        elif instruction == '3':
-            if reverse_instruction[2] == "1":
+            intComputer_part2(commands, pos_to_jump_to, onlyInput, outputs)
+        elif op_code == '3':
+            if instruction[2] == "1":
                 commands[index + 1] = onlyInput
             else:
                 commands[commands[index + 1]] = onlyInput
-            intComputer_part2(commands, index + 2, onlyInput)
-        elif instruction == '4':
-            if reverse_instruction[2] == "1":
-                print(str(commands[index + 1]) + ",")
+            intComputer_part2(commands, index + 2, onlyInput, outputs)
+        elif op_code == '4':
+            if instruction[2] == "1":
+                outputs.append(commands[index + 1])
             else:
-                print(str(commands[commands[index + 1]]) + ",")
-            intComputer_part2(commands, index + 2, onlyInput)
+                outputs.append(commands[commands[index + 1]])
+            intComputer_part2(commands, index + 2, onlyInput, outputs)
         else:
-            first = commands[commands[index+1]
-                             ] if reverse_instruction[2] == '0' else commands[index+1]
-            second = commands[commands[index+2]
-                              ] if reverse_instruction[3] == '0' else commands[index+2]
             pos = commands[index+3]
-            if instruction == '7':
+            if op_code == '7':
                 commands[pos] = 1 if first < second else 0
-                intComputer_part2(commands, index + 4, onlyInput)
-            if instruction == '8':
+                intComputer_part2(commands, index + 4, onlyInput, outputs)
+            if op_code == '8':
                 commands[pos] = 1 if first == second else 0
-                intComputer_part2(commands, index + 4, onlyInput)
-            elif instruction == '1':
+                intComputer_part2(commands, index + 4, onlyInput, outputs)
+            elif op_code == '1':
                 commands[pos] = first + second
-                intComputer_part2(commands, index + 4, onlyInput)
+                intComputer_part2(commands, index + 4, onlyInput, outputs)
             else:
                 # instruction = 2
                 commands[pos] = first * second
-                intComputer_part2(commands, index + 4, onlyInput)
+                intComputer_part2(commands, index + 4, onlyInput, outputs)
 
 
 def intComputer_part1(commands, index, onlyInput):
